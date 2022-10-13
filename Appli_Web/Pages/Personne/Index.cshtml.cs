3fd9877fcbd799573453a37e5e7c15d4921228ce
MatCda <mathieu.ramos@yahoo.fr>
2022-10-13 09:24:03 +0200

    Index - Ordered by name

--- Appli_Web/Pages/Personne/Index.cshtml.cs
+++ Appli_Web/Pages/Personne/Index.cshtml.cs
@@ -14,18 +14,35 @@ namespace Appli_Web.Pages.Personne
     {
         private readonly Appli_Web.Data.Appli_WebContext _context;
 
+        public string NameSort { get; set; }
+
         public IndexModel(Appli_Web.Data.Appli_WebContext context)
         {
             _context = context;
         }
 
-        public IList<Models.Personne> Perosnne { get;set; } = default!;
+        public IList<Models.Personne> Personne { get;set; } = default!;
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string sortOrder)
         {
+            NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            IQueryable<Models.Personne> personne = from s in _context.Personne orderby s.Name
+                                             select s;
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    personne = personne.OrderByDescending(s => s.Name);
+                    break;
+
+                default:
+                    personne = personne.OrderBy(s => s.Name);
+                    break;
+            }
+
             if (_context.Personne != null)
             {
-                Perosnne = await _context.Personne.ToListAsync();
+                Personne = await personne.AsNoTracking().ToListAsync();
             }
         }
     }
